2eba392c64f4f1f4532ded66bce88c127edb958b
toma-iti <watanabe01_2345@yahoo.co.jp>
2023-02-19 23:39:04 +0900

    sub message

--- Earth/Form1.cs
+++ Earth/Form1.cs
@@ -6,5 +6,10 @@ namespace Earth
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("sub sub");
+        }
     }
 }
