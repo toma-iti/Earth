16c71663a8e402d48b8ed1cd796ac175628ab928
toma-iti <watanabe01_2345@yahoo.co.jp>
2023-02-19 23:39:32 +0900

    sub2 message

--- Earth/Form1.cs
+++ Earth/Form1.cs
@@ -10,6 +10,7 @@ namespace Earth
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("sub sub");
+            MessageBox.Show("sub sub2");
         }
     }
 }
